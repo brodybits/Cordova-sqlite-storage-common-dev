b360fee9b50455b269d0958584776c26e3e30b80
Christopher J. Brody <brodybits@litehelpers.net>
2016-12-12 12:44:05 +0100

    Base64 working Windows

--- src/windows/SQLite3-Win-RT/SQLite3/Database.cpp
+++ src/windows/SQLite3-Win-RT/SQLite3/Database.cpp
@@ -3,6 +3,34 @@
 #include "Database.h"
 #include "Statement.h"
 
+extern "C" {
+#include "cencode.h"
+}
+#include <string.h>
+#include <stdint.h>
+static void base64_udf(sqlite3_context * context, int argc, sqlite3_value ** argv) {
+  // TODO check argc
+
+  sqlite3_value * arg = argv[0];
+  const uint8_t * inblob = (const uint8_t *)sqlite3_value_blob(arg);
+  size_t inlen = sqlite3_value_bytes(arg);
+
+  // TBD ???:
+  //char * out = sqlite3_malloc(inlen * 2);
+  //char * out = sqlite3_malloc(1000);
+
+  //sprintf(out, "GOT LENGTH: %d", inlen);
+
+  char * out = (char *)sqlite3_malloc(inlen * 2);
+
+  base64_encodestate es;
+  base64_init_encodestate(&es);
+
+  base64_encode_block((const char *)inblob, inlen, out, &es);
+
+  sqlite3_result_text(context, out, strlen(out), sqlite3_free);
+}
+
 namespace SQLite3
 {
   Database::Database(Platform::String^ dbPath)
@@ -17,6 +45,7 @@ namespace SQLite3
       HRESULT hresult = MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, ret);
       throw ref new Platform::COMException(hresult);
     }
+    sqlite3_create_function(sqlite, "base64", 1, SQLITE_BLOB, NULL, base64_udf, NULL, NULL);
   }
 
   Database::~Database()
